3e729275e43886e11ea127118f2740bf6ded0448
John Carney <jaspajjr@gmail.com>
2017-11-26 16:26:03 +0000

    We can build the sampling tables, and build the context getting functions.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,29 @@
+import numpy as np
+from keras.preprocessing import sequence
 from utilities import collect_data
 
 
 def main():
-    data, count, dictionary, reversed_dict = collect_data()
-    print("Hello, World!")
-    print(data[0:7])
+    vocab_size = 10000
+    data, count, dictionary, reversed_dict = collect_data(vocab_size)
+    window_size = 3
+    vector_dim = 300
+    epochs = 2000000
+    valid_size = 16
+    valid_window = 100
+    valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+
+    sampling_table = sequence.make_sampling_table(vocab_size)
+    couples, labels = sequence.skipgrams(
+            data,
+            vocab_size,
+            window_size=window_size,
+            sampling_table=sampling_table)
+    word_target, word_context = zip(*couples)
+    word_target = np.array(word_target, dtype='int32')
+    word_context = np.array(word_context, dtype='int32')
+
+    print(couples[:10], labels[:10])
 
 
 if __name__ == '__main__':
